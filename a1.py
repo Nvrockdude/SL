cebcda1032b0408efd3acb2b78f566bcc77507e4
Nvrockdude <114585386+Nvrockdude@users.noreply.github.com>
2022-12-28 14:48:53 +0530

    Create a1.py

---
+++ a1.py
@@ -0,0 +1,21 @@
+# Write Python code to do the following:
+# i. Create list with inputs from user
+# ii. Determine minimum and maximum elements in the list
+# iii. Insert new element into the list
+# iv. Delete an element from the list
+# v. Determine if an element is present in the list
+
+li=[]
+n= input("Enter number of elements")
+print("Enter "+ n + " values")
+for i in range(int(n)):
+    li.append(int(input()))
+li.insert(int(input("Enter pos")),int(input("Enter ele")))
+print(li)
+li.remove(int(input("Enter ele to del")))
+print(li)
+pos=li.index(int(input("Find ele")))
+print("Element found at" + str(pos))#Finding the postioin of given element 
+print(li)
+print("MAX:"+str(max(li))+"\nMIN:"+str(min(li)))
+print(li)
